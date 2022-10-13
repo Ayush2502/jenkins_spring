34672998a62457c2e48454453bbe651ce0b38caa
Ayush2502 <49088326+Ayush2502@users.noreply.github.com>
2022-10-13 10:56:04 +0530

    Update UserController.java

--- src/main/java/com/example/JenkinsDemo/Controller/UserController.java
+++ src/main/java/com/example/JenkinsDemo/Controller/UserController.java
@@ -58,7 +58,7 @@ public class UserController {
         else
             userRepository.save(user);
 
-        System.out.println("Hello World 12345");
+        //System.out.println("Hello World 12345");
         System.out.println("Update complete");
         return "User Updated Successfully into the database";
     }
